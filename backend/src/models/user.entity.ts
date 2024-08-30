3ce09ef47eb6cff242ae3d40f4fe374afcaaeb9e
Josh <yujong4170@gmail.com>
2024-08-30 15:38:25 +0900

    yujong

--- backend/src/models/user.entity.ts
+++ backend/src/models/user.entity.ts
@@ -1,4 +1,11 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+// - TypeORM 라이브러리에서 필요한 기능들을 가져옴.
+// - Entity
+//   : 이 데코레이터는 클래스가 DB 테이블로 매핑되도록 지정함.
+// - PrimaryGeneratedColumn
+//   : DB 테이블에 고유한 ID 를 자동으로 생성하는 열 column 을 만듦.
+// - CreateDateColumn
+//   : 이 데코레이터는 DB 행 row 가 생성된 날짜와 시간을 기록하는 열 column 을 만듦
 import { Exclude } from 'class-transformer';
 
 @Entity()
